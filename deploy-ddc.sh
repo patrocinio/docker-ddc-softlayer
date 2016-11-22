0ab1ce1c7bee435becae6f7f388a592883649289
Eduardo Patrocinio <epatro@gmail.com>
2016-11-22 11:39:14 -0500

    Re-enable all the steps

--- deploy-ddc.sh
+++ deploy-ddc.sh
@@ -334,11 +334,11 @@ create_nodes
 update_hosts_file
 
 configure_ucp_primary
-#configure_ucp_secondaries
-#configure_ucps
-#configure_dtr_primary
-#configure_dtr_secondaries
-#configure_nodes
+configure_ucp_secondaries
+configure_ucps
+configure_dtr_primary
+configure_dtr_secondaries
+configure_nodes
 
 echo "Congratulations! You can log in to your Docker Data Center environment at https://$UCP1_IP using admin/orca"
 
